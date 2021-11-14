4405f5ec43731955678193089dea5e286cbaa725
Emirhan <94308664+electr1cc@users.noreply.github.com>
2021-11-14 19:29:35 +0300

    Add files via upload

---
+++ ayarlar.lua
@@ -0,0 +1,10 @@
+-- Client
+panelBaslik = "Reklam Panel  ©electr1c" -- Panelin başlığı.
+panelKomut = "reklam" -- Panelin açılacağı komut.
+panelAciklama = "Aşağıdaki kutucuğa reklam \naçıklaması yapıp reklamı verebilirsiniz." -- Panelde olacak olan açıklama.
+
+-- Mesajlar
+mesaj1 = "Reklam içeriği en az 5 en fazla 140 karakter olabilir." -- Kişinin chatine giden mesajlar.
+mesaj2 = "Reklam verebilmek için en az 5.000$'ınız olması lağzım." 
+mesaj3 = "Reklamınız başarıyla gönderildi!" 
+mesaj4 = "Reklam verebilmek için 5 dakika beklemeniz gerekiyor!"
